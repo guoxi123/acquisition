78a2bbe4ae01b8cf0ba9092aa5b98643c077d163
guoxi <guoxi@guoxideMacBook-Air.local>
2026-09-15 20:35:01 +0800

    Revert utils.py to only contain the divide function

--- utils.py
+++ utils.py
@@ -1,8 +1,2 @@
-def reverse_string(text):
-    return text[::-1]
-
-def count_words(sentence):
-    return len(sentence.split())
-
-def celsius_to_fahrenheit(celsius):
-    return (celsius * 9/5) + 32
+def divide(a, b):
+    return a / b
